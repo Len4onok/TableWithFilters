0e1b4250e8bf4d6ad9400a56cb5577e97cde0b60
Elena <Len4onok@sibmail.com>
2021-05-06 09:00:34 +0700

    added failes

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LogisticTableContainer from './component/LogisticTableContainer';
+
+function App() {
+  return (
+    <div>
+      <LogisticTableContainer/>
+    </div>
+  );
+}
+
+export default App;
